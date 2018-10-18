a21123c594078d28c03f6e5211fba094bbc3360c
mayong <Fw12345678>
2018-10-18 15:50:40 +0800

    提交文档

--- app/src/main/java/com/example/horse/interview/MainActivity.java
+++ app/src/main/java/com/example/horse/interview/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setBackgroundDrawableResource(R.color.transparent);
+//        getWindow().setBackgroundDrawableResource(R.color.transparent);
     }
 
     @Override
